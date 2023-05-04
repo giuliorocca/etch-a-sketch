b3e6cefdc033a1262da5ccbdda42691049d702ad
Giulio <42920670+giuliorocca@users.noreply.github.com>
2023-05-04 22:27:35 +0200

    add js

--- javascript.js
+++ javascript.js
@@ -1,45 +1,30 @@
-const container = document.getElementById('container');
-const square = document.getElementsByClassName('square');
+// Initialize HTML variables
+const container = document.getElementById("container");
+const squares = document.getElementsByClassName("square");
 
-numRows = 16;
-numColumns = 16;
+// Initialize grid size
+const numRows = 16;
+const numColumns = 16;
 
-// Create divs and append them to the container div
-// The container div is set to display: grid so it will
-// automatically arrange the new divs into a grid as specified in CSS
-function createGrid () {
-    for (let i = 0; i < numRows*numColumns; i++) {
+// Create divs that form a grid of rows and columns of divs/cells
+// Create within "container" div, which has "display: grid property"
+function createGrid (numRows, numColumns) {
+    for (let i = 0; i < numRows * numColumns; i++) {
         const div = document.createElement('div');
         div.classList.add('square');
         container.appendChild(div);
     }
 }
 
-square.addEventListener('mouseover', () => {
-    square.style.backgroundColor = 'red';
-    setTimeout(() => {
-        square.style.backgroundColor = '';
-    }, 2000);
-});
-
-
-
-// function createGrid (numRows, numColumns) {
-    
-//     // Create horizontally distributed divs ('rows') for desired grid size
-//     for (i = 0; i < numRows; i++) {
-//         const rowdiv = document.createElement('div');
-//         rowdiv.textContent = 'I am Horiz Div';
-//         rowdiv.setAttribute('class', 'row');
-//         container.appendChild(rowdiv);    
-//     }
-//         // Create vertically distributed divs ('columns') for desired grid size
-//         for (i = 0; i < numColumns; i++) {
-//             const columndiv = document.createElement('div');
-//             columndiv.textContent = 'I am Vert Div';
-//             columndiv.setAttribute('class', 'column');
-//             rowdiv.appendChild(columndiv);
-//         }
-// }
+// Loop through all the HTML elements in squares, which have the "square" class
+// and add a mouseover EventListener to each of them
+for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("mouseover", function () {
+      this.style.backgroundColor = "red";
+      setTimeout(() => {
+        this.style.backgroundColor = "";
+      }, 2000);
+    });
+}
 
 createGrid(numRows, numColumns);

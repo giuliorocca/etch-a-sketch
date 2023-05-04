d80fd412bd10c91af059d4da935dd818436b7e32
Giulio <42920670+giuliorocca@users.noreply.github.com>
2023-05-04 11:45:46 +0200

    change js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// Create a div and stick it in the container div
 const div1 = document.createElement('div');
-div1.textContent = 'Div1';
+div1.textContent = 'I am Div1';
 container.appendChild(div1);

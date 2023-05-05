44dadf211ff065b8fe758840f43da234f968fc06
Giulio <42920670+giuliorocca@users.noreply.github.com>
2023-05-05 17:09:29 +0200

    update

--- javascript.js
+++ javascript.js
@@ -3,15 +3,15 @@ const gridContainer = document.getElementById("gridContainer");
 const gridSize = document.getElementById("gridUserSelection");
 let squares;
 
-// Initialize grid size dimensions based on default drop-down menu selection
+// Initialize grid size dimensions based on default drop-down menu selection 16x16
 let numRows = parseInt(gridSize.value);
 let numColumns = parseInt(gridSize.value);
 
 // Create divs that form a grid of rows and columns
-// Create within "gridContainer" div, which has the "display: grid property"
+// Create divs within "gridContainer" div, which has the "display: grid property"
 function createGrid (numRows, numColumns) {
 
-   // Clear the grid, delete the container's child elements (the div squares created)
+   // Clear the grid, deleting the container's child elements (the div squares created)
    gridContainer.innerHTML = "";
 
     for (let i = 0; i < numRows * numColumns; i++) {
@@ -19,7 +19,7 @@ function createGrid (numRows, numColumns) {
         div.classList.add('square');
         gridContainer.appendChild(div);
     }
-  // Populates the 'squares' variable with all the HTML elements
+  // Populate the 'squares' variable with all the HTML elements
   // tagged with the 'square' class created above
   squares = document.getElementsByClassName("square");
 }
@@ -42,10 +42,10 @@ gridSize.addEventListener("change", () => {
   numRows = parseInt(gridSize.value);
   numColumns = parseInt(gridSize.value);
 
-  // Set the CSS custom properties for the grid size
+  // Update the CSS properties for the grid size
   gridContainer.style.setProperty("--numColumns", numColumns);
   gridContainer.style.setProperty("--numRows", numRows);
 
-  // Call the createGrid function with the new values
+  // Recreate the grid with the new values
   createGrid(numRows, numColumns);
 });

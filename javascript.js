c1d109cb0563e63fdc5f6bd44bd2205e73ef8d11
Giulio <42920670+giuliorocca@users.noreply.github.com>
2023-05-05 16:24:16 +0200

    breaks game but adds evenlistener

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
 // Initialize HTML element variables
 const gridContainer = document.getElementById("gridContainer");
 const gridSize = document.getElementById("gridUserSelection");
-const numRows = gridSize.value;
-const numColumns = gridSize.value;
 let squares;
-console.log(numRows);
+
+// Initialize grid size based on default drop-down menu selection
+let numRows = gridSize.value;
+let numColumns = gridSize.value;
 
 // Create divs that form a grid of rows and columns of divs/cells
 // Create within "gridContainer" div, which has "display: grid property"
@@ -28,6 +29,15 @@ for (let i = 0; i < squares.length; i++) {
       this.style.backgroundColor = "rgb(201, 167, 255)";
       setTimeout(() => {
         this.style.backgroundColor = "";
-      }, 600);
+      }, 800);
     });
 }
+
+// Add an event listener to the drop-down menu where user selects grid size
+gridSize.addEventListener("change", () => {
+  // Update numRows and numColumns variables with new grid size
+  numRows = gridSize.value;
+  numColumns = gridSize.value;
+    // Call the createGrid function with the new values
+    createGrid(numRows, numColumns);
+  });
